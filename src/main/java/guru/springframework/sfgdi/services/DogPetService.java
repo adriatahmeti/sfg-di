8907b774932b553655b803bba76727160758968f
adriatahmeti <adriatahmetii@gmail.com>
2022-11-24 14:20:45 +0100

    di assignemt with errors

---
+++ src/main/java/guru/springframework/sfgdi/services/DogPetService.java
@@ -0,0 +1,11 @@
+package guru.springframework.sfgdi.services;
+
+import org.springframework.context.annotation.Profile;
+
+@Profile({"dog", "default"})
+public class DogPetService {
+
+        public String getPetType(){
+            return "Dogs are the best!";
+        }
+}
